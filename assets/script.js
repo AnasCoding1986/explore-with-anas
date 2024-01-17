607af64f1c9aba3731a2b7c3d482b62c3e67974f
Anas <anassust.scw@gmail.com>
2024-01-17 15:30:07 +0600

    testing

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+let nav = document.getElementsByClassName("nav-menu");
+
+
+document.getElementsByClassName("hamberger-menu").addEventListener('click', function() {
+    let nav = document.getElementsByClassName("nav-menu");
+  
+    // Change the right property to -40px
+    nav.style.right = '-40px';
+  });
+  
